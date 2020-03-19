866ff0322de836dadea8444c88a923f1a826106e
liaowentao <liaowentao@sightp.com>
2020-03-19 18:42:09 +0800

    使用顶点元素数组渲染立方体

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var squareRotation = 0;
+var cubeRotation  = 0;
 function initShaderProgram(gl, vsSource, fsSource) {
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
@@ -49,18 +49,24 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
     mat4.translate(modelViewMatrix,
         modelViewMatrix,
         [-0.0, 0.0, -6.0]);
-    
+
     mat4.rotate(modelViewMatrix,
-                modelViewMatrix,
-                squareRotation,
-                [0,0,1]);
+        modelViewMatrix,
+        cubeRotation,
+        [0, 0, 1]);
+
+    mat4.rotate(modelViewMatrix,
+        modelViewMatrix,
+        cubeRotation,
+        [0, 1, 0]);
 
     const type = gl.FLOAT;
     const normalize = false;
     const stride = 0;
     const offset = 0;
     {
-        const numComponents = 2;
+        //3个buffer组成一个顶点数据
+        const numComponents = 3;
 
         gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
         gl.vertexAttribPointer(
@@ -87,6 +93,9 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
         )
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
     }
+    //Tell WebGL which indices to use to index the vertices
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+
     gl.useProgram(programInfo.program);
 
     gl.uniformMatrix4fv(
@@ -101,38 +110,90 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
     )
     {
         const offset = 0;
-        const vertexCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        const vertexCount = 36;
+        const type = gl.UNSIGNED_SHORT;
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
     }
-    squareRotation += deltaTime;
+    cubeRotation  += deltaTime;
     // 旋转一圈
-    if (squareRotation > Math.PI * 2){
-        squareRotation -= Math.PI * 2;
+    if (cubeRotation  > Math.PI * 2){
+        cubeRotation  -= Math.PI * 2;
     }
 }
 function initBuffers(gl) {
     var vertices = [
-        1.0, 1.0,
-        -1.0, 1.0,
-        1.0, -1.0,
-        -1.0, -1.0,
+        //Front face
+        -1.0, -1.0, 1.0,
+        1.0, -1.0, 1.0,
+        1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+
+        //Back face
+        -1.0, -1.0, -1.0,
+        -1.0, 1.0, -1.0,
+        1.0, 1.0, -1.0,
+        1.0, -1.0, -1.0,
+
+        //Top face
+        -1.0, 1.0, -1.0,
+        -1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+        1.0, 1.0, -1.0,
+        //Bottom face
+        -1.0, -1.0, -1.0,
+        1.0, -1.0, -1.0,
+        1.0, -1.0, 1.0,
+        -1.0, -1.0, 1.0,
+        //Right face
+        1.0, -1.0, -1.0,
+        1.0, 1.0, -1.0,
+        1.0, 1.0, 1.0,
+        1.0, -1.0, 1.0,
+        //Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0, 1.0,
+        -1.0, 1.0, 1.0,
+        -1.0, 1.0, -1.0
     ];
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    
-    const colors = [
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
+
+    var colors = [
+        [1.0, 1.0, 1.0, 1.0],    // Front face: white
+        [1.0, 0.0, 0.0, 1.0],    // Back face: red
+        [0.0, 1.0, 0.0, 1.0],    // Top face: green
+        [0.0, 0.0, 1.0, 1.0],    // Bottom face: blue
+        [1.0, 1.0, 0.0, 1.0],    // Right face: yellow
+        [1.0, 0.0, 1.0, 1.0]     // Left face: purple
     ];
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    var generatedColors = [];
+    for (var j = 0; j < 6; j++) {
+        var c = colors[j];
+        for (var i = 0; i < 4; i++) {
+            generatedColors = generatedColors.concat(c);
+        }
+    }
+    const cubeVerticesColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generatedColors), gl.STATIC_DRAW);
+
+    //顶点渲染索引顺序
+    var cubeVerticesIndices = [
+        0, 1, 2, 0, 2, 3,    // front
+        4, 5, 6, 4, 6, 7,    // back
+        8, 9, 10, 8, 10, 11,   // top
+        12, 13, 14, 12, 14, 15,   // bottom
+        16, 17, 18, 16, 18, 19,   // right
+        20, 21, 22, 20, 22, 23    // left
+    ];
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVerticesIndices), gl.STATIC_DRAW);
     return {
         position: positionBuffer,
-        color: colorBuffer,
+        color: cubeVerticesColorBuffer,
+        indices: indexBuffer,
     }
 }
 function main() {
